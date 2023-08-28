ffa09dbe55b7a76cbbd2f3e9200863daa63a8812
Cindysu123 <cindysu2@illinois.edu>
2023-08-27 21:10:11 -0500

    App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import { HashRouter as Router } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import HomePage from './components/HomePage';
@@ -16,21 +16,38 @@ import './App.css';
 
 const projectComponents = [Project1, Project2, Project3, Project4];
 
+const RouteHandler = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const route = urlParams.get('route');
+
+    if (route) {
+      navigate(route);
+    }
+  }, [navigate]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      {projectComponents.map((Project, index) => (
+        <Route path={`/ui-design/${index + 1}`} element={<Project />} key={index} />
+      ))}
+      <Route path="/ui-design" element={<UIDesignPage />} />
+      <Route path="/product-design" element={<ProductDesignPage />} />
+      <Route path="/drawings" element={<DrawingsPage />} />
+      <Route path="/contact" element={<ContactPage />} />
+    </Routes>
+  );
+};
+
 const App = () => {
   return (
     <div style={{ width: '90vw', height: '90vh', padding: '4vh 4vw' }}>
-      <Router basename="/my-portfolio">
+      <Router>
         <Navigation />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          {projectComponents.map((Project, index) => (
-            <Route path={`/ui-design/${index + 1}`} element={<Project />} key={index} />
-          ))}
-          <Route path="/ui-design" element={<UIDesignPage />} />
-          <Route path="/product-design" element={<ProductDesignPage />} />
-          <Route path="/drawings" element={<DrawingsPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-        </Routes>
+        <RouteHandler />
       </Router>
     </div>
   );
